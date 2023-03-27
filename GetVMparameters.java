c8ba8f095d4612f23896be282897d85c39b5e683
butchsergiy <1144777@gmail.com>
2023-03-27 18:08:11 +0000

    ....

--- GetVMparameters.java
+++ GetVMparameters.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 public class GetVMparameters {
 	public static void main(String[] args) {
 
@@ -8,19 +10,26 @@ public class GetVMparameters {
             " OS Architecture: " + System.getProperty("os.arch") +"\n"+
             " Java Version: " + System.getProperty("java.version")+"\n"
         );
+
         //more OS parameters
         System.out.println(
             "User Home Directory: " + System.getProperty("user.home")+"\n"+
             " User Working Directory: " + System.getProperty("user.dir") +"\n"+
             " User Name: " + System.getProperty("user.name")
         );
+        
         // prints CPU and RAM parameters
         System.out.println(
             "Available Processors: " + Runtime.getRuntime().availableProcessors() + "\n"+
-            " Free Memory: " + Runtime.getRuntime().freeMemory() + "\n"+
-            " Total Memory: " + Runtime.getRuntime().totalMemory()
+            " Free Memory in the JVM (MB): " + Runtime.getRuntime().freeMemory()/1024/1024 + "\n"+
+            " Total Memory in the JVM (MB): " + Runtime.getRuntime().totalMemory()/1024/1024
         );
 
-        
+        File file = new File(".");
+        long freeSpace = file.getFreeSpace();
+        long totalSpace = file.getTotalSpace(); 
+        System.out.println("Free disk space (MB): " + freeSpace/1024/1024);
+        System.out.println("Total space (MB): " + totalSpace/1024/1024);
+
 	}
 }
